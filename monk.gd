b9319aa8396f74fb6da4afafd8b62134abf9f7c1
Nikita Zdanovitch <3hb.tch@gmail.com>
2016-01-30 13:08:43 -0300

    comienzos de codigo sin terminar y carpeta de arte con un placeholder

--- monk.gd
+++ monk.gd
@@ -6,11 +6,32 @@ extends Sprite
 # var b="textvar"
 
 var gravity = 10.0
-var start_y = 0
+var speed = 0.0
+var max_speed = 40.0
+var start_y = 0.0
 
 func _ready():
 	start_y = get_pos().y
 	set_process(true)
 	
 func _process(deltatime):
+	check_input(deltatime)
+	calculate_physics(deltatime)
+	
+func check_input(deltatime):
+	pass	
+
+func calculate_physics(deltatime):
+	var pos = get_pos()
+	
+	speed += gravity * deltatime
+	if abs(speed) > max_speed:
+		speed = max_speed * sign(speed)
+	set_pos(Vector2(pos.x, pos.y + speed * deltatime))
+	
+	pos = get_pos()
+	if pos.y > start_y:
+		speed = 0.0
+		set_pos(Vector2(pos.x, start_y))
+
 	
