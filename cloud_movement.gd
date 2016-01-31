71f7ab258c3eb4468c95d6913e8c27d1834088b1
fnc <fedecroci@hotmail.com>
2016-01-31 11:38:35 -0300

    sprite grieta

--- cloud_movement.gd
+++ cloud_movement.gd
@@ -1,9 +1,8 @@
 var start_position = get_pos()
-var direction = Vector2(1,0)
-var speed = 2
+export var direction = Vector2(1,0)
 
 func _ready():
 	set_process(true)
 
 func _process(deltatime):
-	set_pos(get_pos()+direction*speed*deltatime)
+	set_pos(get_pos()+direction*deltatime)
